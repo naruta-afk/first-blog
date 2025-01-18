16aff008898c8631672b90f9f74a5fbedbbad50e
naruta-afk <affa.15md@gmail.com>
2025-01-18 13:37:15 +0100

    Get Posts

--- backend/Controllers/postController.js
+++ backend/Controllers/postController.js
@@ -32,6 +32,37 @@ fs.unlinkSync(imagePath);
 
   });
 
-module.exports = {createPostCtrl};
+
+  // get all posts
+  const getAllPostsCtrl = asyncHandler(async (req, res) => {
+    const POST_PER_PAGE = 3;
+    const {pageNumber , category}= req.query;
+    let posts;
+    if(pageNumber){
+      posts = await Post.find().skip((pageNumber - 1) * POST_PER_PAGE).limit(POST_PER_PAGE).sort({createdAt : -1}).populate('user',["-password"]);
+    }else if (category){
+      posts = await Post.find({category}).skip((pageNumber - 1) * POST_PER_PAGE).limit(POST_PER_PAGE).sort({createdAt : -1}).populate('user',["-password"]);
+    }else{
+      posts = await Post.find().sort({createdAt : -1}).populate('user',["-password"]);
+    }
+    res.status(200).json(posts);   
+  });
+
+
+  // get single post by id
+  const getPostCtrl = asyncHandler(async (req, res) => {
+    const post = await Post.findById(req.params.id).populate('user',["-password"]);
+    if(!post){ 
+      return res.status(404).json({message:"Post not found"});
+    }
+    res.status(200).json(post);
+  });
+//get post count
+const getPostCountCtrl = asyncHandler(async (req, res) => {
+  const postCount = await Post.countDocuments();
+  res.status(200).json(postCount);
+});
+
+module.exports = {createPostCtrl, getAllPostsCtrl, getPostCtrl, getPostCountCtrl};
 
 
--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const { createPostCtrl } = require('../Controllers/postController');
+const { createPostCtrl, getAllPostsCtrl, getPostCtrl, getPostCountCtrl } = require('../Controllers/postController');
 const photoUpload = require('../middlewares/photoUpload');
 const {verifyToken} = require('../middlewares/verifyToken');
+const validateObject = require('../middlewares/validateObject');
 
-router.route('/').post(verifyToken, photoUpload.single("image"), createPostCtrl);
+router.route('/').post(verifyToken, photoUpload.single("image"), createPostCtrl).get(getAllPostsCtrl);
+//route post count
+router.route('/count').get(getPostCountCtrl);
 
-
+router.route('/:id').get(validateObject,getPostCtrl);
 module.exports = router;
